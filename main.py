cf265ae184299cedd06d038db8dfe5c593d93f0f
thesaurum <80420907+thesaurumshelter@users.noreply.github.com>
2024-10-31 13:53:56 +0100

    add groups

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def format_proxy(proxy_str: str) -> dict | None:
     match_host_port = re.search(reg_host_port, proxy)
     if match_host_port is not None:
         formatted_proxy['host'] = match_host_port.group(1)
-        formatted_proxy['port'] = match_host_port.group(3)
+        formatted_proxy['port'] = match_host_port.group(5)
 
-    reg_user_passw = r"\d*[a-zA-Z]\w+\W\d*[a-zA-Z]\w+"
+    reg_user_passw = r"(\d*[a-zA-Z]\w+)\W(\d*[a-zA-Z]\w+)"
 
     match_user_password = re.search(reg_user_passw, proxy)
     if match_user_password is not None:
         formatted_proxy['user'] = match_user_password.group(1)
-        formatted_proxy['password'] = match_user_password.group(3)
+        formatted_proxy['password'] = match_user_password.group(2)
 
     if formatted_proxy['host'] is None:
         return
