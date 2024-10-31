d22723a67e0c4ebf8100d199d44048f40453266d
thesaurum <80420907+thesaurumshelter@users.noreply.github.com>
2024-10-31 13:41:52 +0100

    change regex

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def format_proxy(proxy_str: str) -> dict | None:
     formatted_proxy['protocol'] = protocol
     formatted_proxy['host'] = None
 
-    reg_host_port = r"((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|01]?[0-9][0-9]?))(:|@)([0-9]{3,4})"
+    reg_host_port = r"(^((2[0-4]\d|25[0-5]|1\d{2}|\d{1,2})\.){3}(2[0-4]\d|25[0-5]|1\d{2}|\d{1,2}))\W([0-9]{3,4})"
     match_host_port = re.search(reg_host_port, proxy)
     if match_host_port is not None:
         formatted_proxy['host'] = match_host_port.group(1)
         formatted_proxy['port'] = match_host_port.group(3)
 
-    reg_user_passw = r"(\w{5,20})(:|@)(\w{5,20})"
+    reg_user_passw = r"\d*[a-zA-Z]\w+\W\d*[a-zA-Z]\w+"
 
     match_user_password = re.search(reg_user_passw, proxy)
     if match_user_password is not None:
